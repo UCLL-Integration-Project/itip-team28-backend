1bd9bf15c50461b1e3660973842a492eb9d0fe15
Nina de Weerd <nina.deweerd@student.ucll.be>
2025-05-14 10:32:17 +0200

    setup backend

---
+++ backend/src/main/java/team28/backend/controller/TestConnection.java
@@ -0,0 +1,16 @@
+package team28.backend.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class TestConnection {
+    @GetMapping
+    public Map<String, String> getStatus() {
+        return Map.of("message", "Backend is running");
+    }
+}
