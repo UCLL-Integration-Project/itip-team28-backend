d7a3a8a4898279b78a43a2f83792c68c30b6d511
T-Van-den-houdt <thomas.vandenhoudt@student.ucll.be>
2025-05-15 11:35:44 +0000

    trying to add test scan data

--- backend/src/main/java/team28/backend/repository/DbInitializer.java
+++ backend/src/main/java/team28/backend/repository/DbInitializer.java
@@ -1,10 +1,13 @@
 package team28.backend.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import team28.backend.model.Role;
+import team28.backend.model.Scan;
 import team28.backend.model.User;
 
 @Component
@@ -12,10 +15,12 @@ public class DbInitializer {
 
     private final UserRepository UserRepository;
     private final PasswordEncoder PasswordEncoder;
+    private final ScanRepository scanRepository;
 
-    public DbInitializer(PasswordEncoder PasswordEncoder, UserRepository UserRepository) {
+    public DbInitializer(PasswordEncoder PasswordEncoder, UserRepository UserRepository, ScanRepository scanRepository) {
         this.PasswordEncoder = PasswordEncoder;
         this.UserRepository = UserRepository;
+        this.scanRepository = scanRepository;
     }
 
     public void clearAll() {
@@ -26,9 +31,12 @@ public class DbInitializer {
     public void init() {
         clearAll();
 
+        @SuppressWarnings("unused")
         final var user = UserRepository
                 .save(new User("test", "test@example.com", PasswordEncoder.encode("test"), Role.USER));
+        @SuppressWarnings("unused")
         final var user2 = UserRepository
                 .save(new User("test2", "test@example.com", PasswordEncoder.encode("test"), Role.MANAGER));
+        final var scan1 = ScanRepository.save(new Scan("1", "macaddr1", LocalDateTime.of(2025, 5, 1, 9, 15)))
     }
 }
