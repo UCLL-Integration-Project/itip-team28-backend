02eade0b360672019f0293b3d9df096aecab4abf
Matvii Rubets <matviirubets@Matviis-MacBook-Pro.local>
2025-05-15 13:41:20 +0200

    some config stuff

--- backend/src/main/java/team28/backend/BackendApplication.java
+++ backend/src/main/java/team28/backend/BackendApplication.java
@@ -2,8 +2,13 @@ package team28.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import team28.backend.config.CorsProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
+
 public class BackendApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/team28/backend/config/WebConfig.java
@@ -0,0 +1,20 @@
+package team28.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:8080",  // Local frontend
+                "http://localhost:3000")  // Local backend
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Content-Type", "Authorization")
+            .allowCredentials(true);
+    }
+}
