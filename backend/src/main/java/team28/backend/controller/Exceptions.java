6640363bb23b67c74e44ac21490bad1904b20fed
Nina de Weerd <nina.deweerd@student.ucll.be>
2025-05-15 13:11:07 +0200

    added extra error

--- backend/src/main/java/team28/backend/controller/Exceptions.java
+++ backend/src/main/java/team28/backend/controller/Exceptions.java
@@ -1,12 +1,15 @@
 package team28.backend.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import team28.backend.exceptions.UserException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -17,6 +20,15 @@ public class Exceptions {
         return Map.of(
                 "status", "Service error",
                 "message", UserException.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseBody
+    public Map<String, String> handleBadCredentials(BadCredentialsException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("errorMessage", "Invalid username or password");
+        return error;
     }
 
 }
