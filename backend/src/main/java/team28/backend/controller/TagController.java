549144788c0ddcc186f053c154a321c545cfd269
Cas-Marien <cas.marien@student.ucll.be>
2025-05-16 11:51:53 +0200

    Data from influxdb via tag service file

--- backend/src/main/java/team28/backend/controller/TagController.java
+++ backend/src/main/java/team28/backend/controller/TagController.java
@@ -1,26 +1,31 @@
 package team28.backend.controller;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import team28.backend.model.Tag;
 import team28.backend.service.TagService;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tags")
 public class TagController {
 
-    private final TagService TagService;
+    private final TagService tagService;
 
-    public TagController(TagService TagService) {
-        this.TagService = TagService;
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
     }
 
     @GetMapping
-    public List<Tag> GetAllTags() {
-        return TagService.GetAllTags();
+    public List<Tag> getAllTags() {
+        return tagService.GetAllTags();
+    }
+
+    @GetMapping("/data")
+    public List<Map<String, String>> getTagData() {
+        return tagService.getTagDataFromInflux();
     }
 }
